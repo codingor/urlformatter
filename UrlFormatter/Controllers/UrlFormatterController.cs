32a91f679a228bb1ca5d700c6883e6cb5bd4c70c
Kevin Fan <kevin.fan@finning.com>
2020-10-02 15:44:43 -0700

    Create a controller endpoint.

--- UrlFormatter/Controllers/UrlFormatterController.cs
+++ UrlFormatter/Controllers/UrlFormatterController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using UrlFormatter.Requests;
 
 namespace UrlFormatter.Controllers
 {
@@ -10,6 +12,21 @@ namespace UrlFormatter.Controllers
     [Route("api/v1.0/url")]
     public class UrlFormatterController : ControllerBase
     {
-        
+        private readonly IMediator _mediator;
+
+        public UrlFormatterController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string originalUrl)
+        {
+            var result = await _mediator.Send(new UrlFormatterRequest
+            {
+                OriginalUrl = originalUrl
+            });
+            return Ok(result);
+        }
     }
 }
