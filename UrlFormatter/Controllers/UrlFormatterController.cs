cc2eac0860eea72abd3f8b559161c0809f2dd0d0
Kevin Fan <kevin.fan@finning.com>
2020-10-02 14:41:51 -0700

    Add controller and request

---
+++ UrlFormatter/Controllers/UrlFormatterController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UrlFormatter.Controllers
+{
+    [ApiController]
+    [Route("api/v1.0/url")]
+    public class UrlFormatterController : ControllerBase
+    {
+        
+    }
+}
---
+++ UrlFormatter/Requests/UrlFormatterRequest.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UrlFormatter.Requests
+{
+    public class UrlFormatterRequest : IRequest<string>
+    {
+        public string OriginalUrl { get; set; }
+    }
+
+    public class UrlFormatterRequestHandler : IRequestHandler<UrlFormatterRequest, string>
+    {
+        public Task<string> Handle(UrlFormatterRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
